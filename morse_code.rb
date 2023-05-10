65892c8d69616151a5b7748425dcb2cc0ef09b1b
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-05-10 18:21:37 +0000

    Fixed linter errors

--- morse_code.rb
+++ morse_code.rb
@@ -34,15 +34,31 @@ class MorseCodeDecoder
 
   def self.decode_word(word)
     decoded_word = ''
-    characters = word.split(' ')
+    characters = word.split
     characters.each { |char| decoded_word << decode_char(char) }
     decoded_word
   end
 
   def self.decode(message)
     decoded_message = ''
-    words = message.split('')
+    words = message.split
     words.each { |word| decoded_message << decode_word(word) << ' ' }
     decoded_message.strip
   end
 end
+
+character = '.-'
+decoded_character = MorseCodeDecoder.decode_char(character)
+puts decoded_character
+
+word = '-- -.--'
+decoded_word = MorseCodeDecoder.decode_word(word)
+puts decoded_word
+
+message1 = '-- -.--   -. .- -- .'
+decoded_message1 = MorseCodeDecoder.decode(message1)
+puts decoded_message1
+
+message2 = '      .-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+decoded_message2 = MorseCodeDecoder.decode(message2)
+puts decoded_message2
